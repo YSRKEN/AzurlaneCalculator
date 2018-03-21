64e65b6cb5709430360205270cb6284f5c5fc10e
YSR <ysr.ken@gmail.com>
2018-03-21 22:03:42 +0900

    海域周回回数を計算するロジックも作って、UIも整え直した

--- AzurlaneCalculator/AzurlaneCalculator/Stores/CalcExp.cs
+++ AzurlaneCalculator/AzurlaneCalculator/Stores/CalcExp.cs
@@ -30,10 +30,8 @@ namespace AzurlaneCalculator.Stores
 		// 海域の経験値情報
 		private struct MapExp
 		{
-			// 章番号
-			public int Area;
-			// 海域番号
-			public int Stage;
+			// 海域名
+			public string StageName;
 			// 小型艦隊の基礎経験値
 			public int EnemyExpS;
 			// 中型艦隊の基礎経験値
@@ -75,17 +73,16 @@ namespace AzurlaneCalculator.Stores
 				while ((line = reader.ReadLine()) != null) {
 					// データ行かを判定する
 					string[] temp = line.Split("\t".ToCharArray());
-					if (temp[0] == "章")
+					if (temp[0] == "海域")
 						continue;
 					// 数値を読み取り、海域の経験値表に追記する
 					var mapExp = new MapExp {
-						Area = int.Parse(temp[0]),
-						Stage = int.Parse(temp[1]),
-						EnemyExpS = int.Parse(temp[2]),
-						EnemyExpM = int.Parse(temp[3]),
-						EnemyExpL = int.Parse(temp[4]),
-						EnemyExpB = int.Parse(temp[5]),
-						EnemyCount = int.Parse(temp[6]),
+						StageName = temp[0],
+						EnemyExpS = int.Parse(temp[1]),
+						EnemyExpM = int.Parse(temp[2]),
+						EnemyExpL = int.Parse(temp[3]),
+						EnemyExpB = int.Parse(temp[4]),
+						EnemyCount = int.Parse(temp[5]),
 					};
 					output.Add(mapExp);
 				}
@@ -102,6 +99,32 @@ namespace AzurlaneCalculator.Stores
 			if (level < MinLevel || level > MaxLevel)
 				return -1;
 			return levelExpList[level - 1];
+		}
+		// 海域名のリスト
+		public static List<string> StageNameList {
+			get {
+				return mapExpList.Select(m => m.StageName).ToList();
+			}
+		}
+		// ある海域における獲得経験値を取得する
+		public static int StageExp(string stageName, string enemyType) {
+			// 海域情報を取得する
+			var mapInfo = mapExpList.Where(m => m.StageName == stageName).First();
+			// 敵の種類によって場合分けする
+			switch (enemyType) {
+			case "小型":
+				return mapInfo.EnemyExpS;
+			case "中型":
+				return mapInfo.EnemyExpM;
+			case "大型":
+				return mapInfo.EnemyExpL;
+			case "ボス":
+				return mapInfo.EnemyExpB;
+			case "周回":
+				return mapInfo.AverageEnemyExp;
+			default:
+				return 0;
+			}
 		}
 	}
 }
--- AzurlaneCalculator/AzurlaneCalculator/ViewModels/CalcExpPageViewModel.cs
+++ AzurlaneCalculator/AzurlaneCalculator/ViewModels/CalcExpPageViewModel.cs
@@ -15,46 +15,55 @@ namespace AzurlaneCalculator.ViewModels
 		#pragma warning disable 0067
 		public event PropertyChangedEventHandler PropertyChanged;
 
-		// 必要経験値を計算する
-		private string ExpLevelToLevelStr()
-		{
-			// レベルを取得
-			int startLevel = LevelList[StartLevelIndex.Value];
-			int goalLevel = LevelList[GoalLevelIndex.Value];
-			// 入力チェック
-			if (startLevel < CalcExp.MinLevel || startLevel > CalcExp.MaxLevel)
-				return "<None>";
-			if (goalLevel < CalcExp.MinLevel || goalLevel > CalcExp.MaxLevel)
-				return "<None>";
-			// 結果を返す
-			return $"{CalcExp.LevelExp(goalLevel) - CalcExp.LevelExp(startLevel)}";
-		}
-
 		// ReactiveProperty
-		public ReactiveProperty<int> StartLevelIndex { get; }
-			= new ReactiveProperty<int>(0);
-		public ReactiveProperty<int> GoalLevelIndex { get; }
-			= new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> StartLevel { get; }
+			= new ReactiveProperty<int>(1);
+		public ReactiveProperty<int> GoalLevel { get; }
+			= new ReactiveProperty<int>(1);
 		public ReadOnlyReactiveProperty<string> OutputText { get; }
-		// ObservableCollection
-		public ObservableCollection<int> LevelList { get; }
+		public ReactiveProperty<string> StageName { get; }
+			= new ReactiveProperty<string>("1-1");
+		public ReactiveProperty<string> EnemyType { get; }
+			= new ReactiveProperty<string>("周回");
+		// コレクション
+		public List<int> LevelList { get; }
+		public List<string> StageNameList { get; }
+		public List<string> EnemyTypeList { get; }
+			= new List<string> { "小型", "中型", "大型", "ボス", "周回" };
 
 		// コンストラクタ
 		public CalcExpPageViewModel()
 		{
-			// ReactiveCollectionを設定
+			// コレクションを設定
 			{
-				var levelList = new List<int>();
+				LevelList = new List<int>();
 				for(int i = CalcExp.MinLevel; i <= CalcExp.MaxLevel; ++i)
 				{
-					levelList.Add(i);
+					LevelList.Add(i);
 				}
-				LevelList = new ObservableCollection<int>(levelList);
+			}
+			{
+				StageNameList = CalcExp.StageNameList;
 			}
 			// ReadOnlyReactivePropertyを設定
-			OutputText = StartLevelIndex.CombineLatest(
-				GoalLevelIndex,
-				(s, g) => $"必要経験値：{ExpLevelToLevelStr()}"
+			OutputText = StartLevel.CombineLatest(
+				GoalLevel, StageName, EnemyType, (sl, gl, sn, et) => {
+					string output = "";
+					int startExp = CalcExp.LevelExp(sl);
+					int goalExp = CalcExp.LevelExp(gl);
+					output += $"必要経験値：{goalExp - startExp}";
+					int wantExp = goalExp - startExp;
+					int getExp = CalcExp.StageExp(sn, et);
+					if (getExp != 0) {
+						output += $"\n取得経験値：{getExp}";
+						output += $"\n必要回数：{Math.Ceiling(1.0 * wantExp / getExp)}";
+					}
+					else {
+						output += "\n取得経験値：―";
+						output += "\n必要回数：―";
+					}
+					return output;
+				}
 			).ToReadOnlyReactiveProperty();
 		}
 	}
