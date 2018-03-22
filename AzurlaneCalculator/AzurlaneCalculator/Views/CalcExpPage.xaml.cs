3b679cf0806e989587f9b8d06ad3f13dc5658fbd
YSR <ysr.ken@gmail.com>
2018-03-22 23:28:36 +0900

    確率計算機のUIをあらかた書いた

--- AzurlaneCalculator/AzurlaneCalculator/Views/CalcExpPage.xaml.cs
+++ AzurlaneCalculator/AzurlaneCalculator/Views/CalcExpPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using AzurlaneCalculator.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace AzurlaneCalculator.Views
 {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CalcExpPage : TabbedPage
 	{
 		public CalcExpPage()
--- AzurlaneCalculator/AzurlaneCalculator/Views/CalcSkillPage.xaml.cs
+++ AzurlaneCalculator/AzurlaneCalculator/Views/CalcSkillPage.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using AzurlaneCalculator.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AzurlaneCalculator.Views
 {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CalcSkillPage : ContentPage
 	{
 		public CalcSkillPage ()
