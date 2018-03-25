bc98ac550d12710d0e0f5f0ed8d3eee2a3fcd07a
YSR <ysr.ken@gmail.com>
2018-03-26 00:48:13 +0900

    そう言えばSubcribeできたわここ

--- AzurlaneCalculator/AzurlaneCalculator/Views/DetailPage.xaml.cs
+++ AzurlaneCalculator/AzurlaneCalculator/Views/DetailPage.xaml.cs
@@ -14,13 +14,9 @@ namespace AzurlaneCalculator.Views
 			// 選択を切り替えた際の動きを記述する
 			var masterPage = this.Master as MasterPage;
 			var masterPageViewModel = masterPage.BindingContext as MasterPageViewModel;
-			masterPageViewModel.SelectedMenuItem.PropertyChanged += (s, a) => {
-				// MasterPageのViewModelで何かが変化した際の動作
-				// まず選択されているアイテムを取得する
-				var item = masterPageViewModel.SelectedMenuItem.Value;
+			masterPageViewModel.SelectedMenuItem.Subscribe(item => {
 				// アイテムがnullでなければ、それに合わせて表示するページを切り替える
-				if (item != null)
-				{
+				if (item != null) {
 					// itemに登録したTargetTypeから表示ページのインスタンスを作成し、代入する
 					Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
 					// 選択を解除する
@@ -28,7 +24,7 @@ namespace AzurlaneCalculator.Views
 					// 選択ページを引っ込める
 					IsPresented = false;
 				}
-			};
+			});
 		}
 	}
 }
