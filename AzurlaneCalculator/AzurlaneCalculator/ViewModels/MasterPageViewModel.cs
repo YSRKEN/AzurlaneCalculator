e81a67dbc1a283d49b0dd77ddbea496e85518571
YSR <ysr.ken@gmail.com>
2018-03-26 00:15:59 +0900

    MasterPageにBindingするクラスの名前を変更した

--- AzurlaneCalculator/AzurlaneCalculator/ViewModels/MasterPageViewModel.cs
+++ AzurlaneCalculator/AzurlaneCalculator/ViewModels/MasterPageViewModel.cs
@@ -1,6 +1,6 @@
-﻿using AzurlaneCalculator.Models;
-using AzurlaneCalculator.Views;
+﻿using AzurlaneCalculator.Views;
 using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -13,10 +13,10 @@ namespace AzurlaneCalculator.ViewModels
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		// ReactiveProperty
-		public ReactiveProperty<MenuItem> SelectedMenuItem { get; set; }
-			= new ReactiveProperty<MenuItem>();
+		public ReactiveProperty<MasterMenuItem> SelectedMenuItem { get; set; }
+			= new ReactiveProperty<MasterMenuItem>();
 		// ReactiveCollection
-		public ReadOnlyReactiveCollection<MenuItem> MenuList { get; }
+		public ReadOnlyReactiveCollection<MasterMenuItem> MenuList { get; }
 
 		// コンストラクタ
 		public MasterPageViewModel()
@@ -24,16 +24,21 @@ namespace AzurlaneCalculator.ViewModels
 			#region ReactiveCollectionを設定
 			// MenuList
 			{
-				var menuList = new List<MenuItem> {
-				new MenuItem {Title = "艦船経験値計算機", TargetType = typeof(CalcExpPage) },
-				new MenuItem {Title = "艦船スキル計算機", TargetType = typeof(CalcSkillPage) },
-				new MenuItem {Title = "確率計算機", TargetType = typeof(CalcProbPage) },
-				new MenuItem {Title = "このアプリについて", TargetType = typeof(AboutPage) },
+				var menuList = new List<MasterMenuItem> {
+				new MasterMenuItem {Title = "艦船経験値計算機", TargetType = typeof(CalcExpPage) },
+				new MasterMenuItem {Title = "艦船スキル計算機", TargetType = typeof(CalcSkillPage) },
+				new MasterMenuItem {Title = "確率計算機", TargetType = typeof(CalcProbPage) },
+				new MasterMenuItem {Title = "このアプリについて", TargetType = typeof(AboutPage) },
 			};
-				var oc = new ObservableCollection<MenuItem>(menuList);
+				var oc = new ObservableCollection<MasterMenuItem>(menuList);
 				MenuList = oc.ToReadOnlyReactiveCollection();
 			}
 			#endregion
 		}
+	}
+	class MasterMenuItem
+	{
+		public string Title { get; set; }
+		public Type TargetType { get; set; }
 	}
 }
